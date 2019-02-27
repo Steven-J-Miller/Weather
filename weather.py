e16d42380fb540fb3d9669968947d1f6233a6079
Steven-J-Miller <mearnhardtfan@gmail.com>
2019-02-27 18:52:19 -0500

    Added documentation, some polishing up

--- weather.py
+++ weather.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 
 def get_weather_city(city, key):
+    """Returns dictionary item of current weather of city by city name"""
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=imperial'
     try:
         r = requests.get(url)
@@ -20,6 +21,7 @@ def get_weather_city(city, key):
 
 
 def get_weather_zip(zip_code, key, country='us'):
+    """Returns dictionary item of current weather of city by zip code"""
     url = f'http://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country}&appid={key}&units=imperial'
     try:
         r = requests.get(url)
@@ -31,6 +33,7 @@ def get_weather_zip(zip_code, key, country='us'):
 
 
 def get_forecast_city(city, key, country='us'):
+    """Returns dictionary item of weather forecast of city by city name"""
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&appid={key}&units=imperial'
     try:
         r = requests.get(url)
@@ -42,6 +45,7 @@ def get_forecast_city(city, key, country='us'):
 
 
 def get_forecast_zip(zip_code, key, country='us'):
+    """Returns dictionary item of weather forecast of city by zip code"""
     url = f'http://api.openweathermap.org/data/2.5/forecast?zip={zip_code},{country}&appid={key}&units=imperial'
     try:
         r = requests.get(url)
@@ -53,7 +57,11 @@ def get_forecast_zip(zip_code, key, country='us'):
 
 
 def parse_weather(weather):
-    """Weather, temp, pressure, humidity, wind, rainfall"""
+    """Prints weather information"""
+    if weather['cod'] == '404':  # returns error message if API call doesn't find valid city
+        print(weather['message'])
+        return 0
+
     city = weather['name']
     country = weather['sys']['country']
     desc = weather['weather'][0]['main']
@@ -62,7 +70,6 @@ def parse_weather(weather):
     temp_c = (temp - 32)/1.8
     temp_f = temp
 
-    pressure = weather['main']['pressure']
     humidity = weather['main']['humidity']
 
     sunrise = datetime.fromtimestamp(weather['sys']['sunrise'])
@@ -70,7 +77,7 @@ def parse_weather(weather):
     sunset = datetime.fromtimestamp(weather['sys']['sunset'])
     sunset = datetime.strftime(sunset, '%I:%M %p')
 
-    if 'rain' in weather.keys():
+    if 'rain' in weather.keys():  # gets rainfall total if data exists, otherwise 0
         if len(weather['rain']) > 0:
             rain = weather['rain']['1h']
     else:
@@ -89,9 +96,13 @@ def parse_weather(weather):
 
 
 def parse_forecast(forecast):
+    """Prints forecast dictionary into a table"""
+    if forecast['cod'] == '404':  # returns error message if API call doesn't find valid city
+        print(forecast['message'])
+        return 0
     print(f"Forecast for {forecast['city']['name']}")
     print('-'*80)
-    print('|   Date    |   Time   | Temp|  Weather  |   Wind   | Cloud %  |   Rainfall    |')
+    print('|   Date    |   Time   | Temp|   Weather  |   Wind   | Cloud %  |   Rainfall   |')
     print('|'+'='*78+'|')
     for row in forecast['list']:
         time = datetime.fromtimestamp(row['dt'])
@@ -107,27 +118,30 @@ def parse_forecast(forecast):
         temp = f"{row['main']['temp']:.0f}°"
         weather_status = row['weather'][0]['main']
         wind = f"{row['wind']['speed']:.1f} mph"
-        print(f'| {time} | {temp} | {weather_status:^9} | {wind:^8} | {cloud_cover:^8} | {rain:^13} |')
+        print(f'| {time} | {temp:^4} | {weather_status:^9} | {wind:^8} | {cloud_cover:^8} | {rain:^12} |')
     print('-' * 80)
 
 
 if __name__ == "__main__":
     api_key = open('key.txt', 'r').read()
     while True:
-        location = input("Welcome! To receive weather information please enter a zip code or city name: ")
+        location = input("Welcome! To receive weather information please enter a zip code or city name, or 'exit' to exit: ")
+
+        if location == 'exit':
+            print("Thanks for stopping by!")
+            break
+
         weather_info = input("Would you like to receive (c)urrent weather, a (f)orecast, or (b)oth? Use 'c', 'f', or 'b' to choose: ")
-        # TODO: call appropriate functions based on user input, check only first letter of input. Loop through again if wrong
-        to_file = input("Would you like to save your weather information to a file? ")
-        # TODO: input checking yes or no to question, add to_file=False attribute to parse functions
+
         if location.isnumeric():
-            if weather_info[0] == 'w' or weather_info[0] == 'b':
+            if weather_info[0] == 'c' or weather_info[0] == 'b':
                 weather = get_weather_zip(location, api_key)
                 parse_weather(weather)
             if weather_info[0] == 'f' or weather_info[0] == 'b':
                 forecast = get_forecast_zip(location, api_key)
                 parse_forecast(forecast)
         else:
-            if weather_info[0] == 'w' or weather_info[0] == 'b':
+            if weather_info[0] == 'c' or weather_info[0] == 'b':
                 weather = get_weather_city(location, api_key)
                 parse_weather(weather)
             if weather_info[0] == 'f' or weather_info[0] == 'b':
